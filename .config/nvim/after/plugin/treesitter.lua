12791c317707f18501f3b7f4d07b16cb791a401b
Yuri Ratanov <organium@gmail.com>
2023-04-21 22:36:33 +0100

    fix treesitter for large files

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,19 +1,10 @@
 require 'nvim-treesitter.configs'.setup {
-  -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-  disable = function(lang, buf)
-    local max_filesize = 100 * 1024     -- 100 KB
-    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-    if ok and stats and stats.size > max_filesize then
-      return true
-    end
-  end,
-
   endwise = {
         enable = true,
     },
 
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "help", "query", "javascript", "typescript", "ruby", "markdown", "markdown_inline" },
+  ensure_installed = { "sql", "lua", "vim", "help", "query", "javascript", "typescript", "ruby", "markdown", "markdown_inline" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
@@ -30,6 +21,13 @@ require 'nvim-treesitter.configs'.setup {
 
   highlight = {
     enable = true,
+     disable = function(lang, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+            return true
+        end
+    end,
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
