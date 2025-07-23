a48eaa52a27d20619556781efaff09f270f68880
Yuri Ratanov <organium@gmail.com>
2025-07-23 20:16:34 +0100

    rice me up

---
+++ dot-config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,35 @@
+return {
+	{
+		"stevearc/conform.nvim",
+		event = { "BufWritePre" },
+		cmd = { "ConformInfo" },
+		keys = {
+			{
+				"<leader>fo",
+				function()
+					require("conform").format({ async = true, lsp_fallback = true })
+				end,
+				mode = "",
+				desc = "Format buffer",
+			},
+		},
+		-- Everything in opts will be passed to setup()
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+				ruby = { "prettierd" },
+				eruby = { "erb_format" },
+				javascript = { "prettierd" },
+				typescript = { "prettierd" },
+				handlebars = { "prettierd" },
+			},
+			formatters = {
+				prettier = {
+					condition = false,
+				},
+			},
+			-- Set up format-on-save
+			format_on_save = false,
+		},
+	},
+}
