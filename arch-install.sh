c6f82f1bb27915cf289d5865938883cfa2d5ae13
Yuri Ratanov <organium@gmail.com>
2025-09-03 16:03:37 +0100

    add basic installations script for arch

---
+++ arch-install.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+
+# yay
+sudo pacman -Syu --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
+
+yay -Syu --noconfirm \
+  hyprland waybar hyprlock hypridle hyprpaper wl-clipboard pavucontrol clipse sddm wl-clip-persist walker-bin hyprland-per-window-layout mako qt5-wayland qt6-wayland noto-fonts noto-fonts-extra noto-fonts-emoji woff2-font-awesome xdg-desktop-portal-gtk xdg-desktop-portal-hyprland \
+  gnome-calculator gnome-weather fontconfig gnome-keyring gnome-themes-extra noto-fonts-cjk noto-fonts-emoji noto-fonts-extra yaru-icon-theme \
+  \
+  swappy hyprshot wl-screenrec wf-recorder slurp \
+  \
+  alacritty ghostty nautilus nextcloud-client fzf starship fd man \
+  \
+  firefox chromium thunderbird \
+  \
+  bitwarden 1password-cli 1password-beta \
+  \
+  neovim mise lazygit docker docker-compose ttf-jetbrains-mono-nerd stylua arduino-cli lazydocker imagemagick github-cli clang \
+  \
+  mpv youtube-music vlc imv \
+  \
+  discord telegram-desktop slack-desktop-wayland \
+  \
+  neofetch btop gnome-disk-utility openvpn \
+  \
+  dbeaver \
+  \
+  freecad libreoffice bambustudio-bin prusa-slicer
+  \
+  pipewire pipewire-pulse pipewire-jack wireplumber
+
+# Open all images with imv
+xdg-mime default imv.desktop image/png
+xdg-mime default imv.desktop image/jpeg
+xdg-mime default imv.desktop image/gif
+xdg-mime default imv.desktop image/webp
+xdg-mime default imv.desktop image/bmp
+xdg-mime default imv.desktop image/tiff
+
+# Open PDFs with the Document Viewer
+xdg-mime default org.gnome.Evince.desktop application/pdf
+
+# Use Chromium as the default browser
+xdg-settings set default-web-browser chromium.desktop
+xdg-mime default chromium.desktop x-scheme-handler/http
+xdg-mime default chromium.desktop x-scheme-handler/https
+
+# Open video files with mpv
+xdg-mime default vlc.desktop video/mp4
+xdg-mime default vlc.desktop video/x-msvideo
+xdg-mime default vlc.desktop video/x-matroska
+xdg-mime default vlc.desktop video/x-flv
+xdg-mime default vlc.desktop video/x-ms-wmv
+xdg-mime default vlc.desktop video/mpeg
+xdg-mime default vlc.desktop video/ogg
+xdg-mime default vlc.desktop video/webm
+xdg-mime default vlc.desktop video/quicktime
+xdg-mime default vlc.desktop video/3gpp
+xdg-mime default vlc.desktop video/3gpp2
+xdg-mime default vlc.desktop video/x-ms-asf
+xdg-mime default vlc.desktop video/x-ogm+ogg
+xdg-mime default vlc.desktop video/x-theora+ogg
+xdg-mime default vlc.desktop application/ogg
+
+mise settings add idiomatic_version_file_enable_tools ruby
+
+# Increase lockout limit to 10 and decrease timeout to 2 minutes
+sudo sed -i 's|^\(auth\s\+required\s\+pam_faillock.so\)\s\+preauth.*$|\1 preauth silent deny=10 unlock_time=120|' "/etc/pam.d/system-auth"
+sudo sed -i 's|^\(auth\s\+\[default=die\]\s\+pam_faillock.so\)\s\+authfail.*$|\1 authfail deny=10 unlock_time=120|' "/etc/pam.d/system-auth"
+
+# Configure Docker daemon:
+# - limit log size to avoid running out of disk
+# - use host's DNS resolver
+sudo mkdir -p /etc/docker
+sudo tee /etc/docker/daemon.json >/dev/null <<'EOF'
+{
+    "log-driver": "json-file",
+    "log-opts": { "max-size": "10m", "max-file": "5" },
+    "dns": ["172.17.0.1"],
+    "bip": "172.17.0.1/16"
+}
+EOF
+
+# Expose systemd-resolved to our Docker network
+sudo mkdir -p /etc/systemd/resolved.conf.d
+echo -e '[Resolve]\nDNSStubListenerExtra=172.17.0.1' | sudo tee /etc/systemd/resolved.conf.d/20-docker-dns.conf >/dev/null
+sudo systemctl restart systemd-resolved
+
+# Start Docker automatically
+sudo systemctl enable docker
+
+# Give this user privileged Docker access
+sudo usermod -aG docker ${USER}
+
+# Prevent Docker from preventing boot for network-online.target
+sudo mkdir -p /etc/systemd/system/docker.service.d
+sudo tee /etc/systemd/system/docker.service.d/no-block-boot.conf <<'EOF'
+[Unit]
+DefaultDependencies=no
+EOF
+
+sudo systemctl daemon-reload
+
+gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
+gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
+gsettings set org.gnome.desktop.interface icon-theme "Yaru-blue"
+
+# Set links for Nautilius action icons
+sudo ln -snf /usr/share/icons/Adwaita/symbolic/actions/go-previous-symbolic.svg /usr/share/icons/Yaru/scalable/actions/go-previous-symbolic.svg
+sudo ln -snf /usr/share/icons/Adwaita/symbolic/actions/go-next-symbolic.svg /usr/share/icons/Yaru/scalable/actions/go-next-symbolic.svg
+sudo gtk-update-icon-cache /usr/share/icons/Yaru
+
+# Ensure iwd service will be started
+sudo systemctl enable iwd.service
+
+# Prevent systemd-networkd-wait-online timeout on boot
+sudo systemctl disable systemd-networkd-wait-online.service
+sudo systemctl mask systemd-networkd-wait-online.service
