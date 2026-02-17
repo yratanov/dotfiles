7cc3604310342b30645198e094fe2e4b767132fc
Yuri Ratanov <organium@gmail.com>
2026-02-17 12:54:20 +0000

    fix test runner

--- dot-config/nvim/scripts/run_tests.sh
+++ dot-config/nvim/scripts/run_tests.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-if ! tmux list-panes -F '#{pane_index}' | grep -q "$2"; then
+TARGET_PANE="$2"
+
+if ! tmux list-panes -F '#{pane_index}' | grep -q "^${TARGET_PANE}$"; then
   tmux split-window -dh -l 40%
+  sleep 0.1
 fi
 
-tmux send -t "$2" C-c
-tmux send -t "$2" -X cancel
-tmux send -t "$2" clear Enter
-tmux send -t "$2" "$1" Enter
+tmux send-keys -t "$TARGET_PANE" C-c
+tmux send-keys -t "$TARGET_PANE" C-u
+tmux send-keys -t "$TARGET_PANE" "clear" Enter
+tmux send-keys -t "$TARGET_PANE" "$1" Enter
 
